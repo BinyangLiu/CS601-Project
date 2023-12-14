26b8b77b34a8133d731a7763e7fbc02a886a6408
Binyang Liu <novemberseventeenth5@gmail.com>
2023-12-14 03:21:14 -0500

    Add files via upload

---
+++ vod.js
@@ -0,0 +1,21 @@
+
+document.getElementById("vod1").addEventListener("mouseover", function() 
+{
+	this.play();
+});
+
+document.getElementById("vod1").addEventListener("mouseleave", function() 
+{
+	this.pause();
+});
+document.getElementById("vod2").addEventListener("mouseover", function() 
+{
+	this.play();
+});
+
+document.getElementById("vod2").addEventListener("mouseleave", function() 
+{
+	this.pause();
+});
+
+
---
+++ vue.js
@@ -0,0 +1,100 @@
+//<iframe src="https://store.steampowered.com/widget/236850/" frameborder="0" width="646" height="190"></iframe>
+new Vue({
+    el: '#app1',
+    data() {
+      return { 
+       
+       
+        image1:{
+          id:1,
+          src:"./media/eu4.jpg",
+          alt:"https://store.steampowered.com/widget/236850/" ,
+          src2:"./media/eu4.jpg",
+          
+
+        }
+        
+      }
+    },
+    
+    
+  });
+  new Vue({
+    el: '#app2',
+    data() {
+      return { 
+       
+       
+       
+        image2:{
+            id:2,
+            src:"./media/stellaris.jpg",
+            alt:"https://store.steampowered.com/widget/281990/38760/" ,
+            src2:"./media/stellaris.jpg"
+          },
+          
+          
+      }
+    },
+    
+    
+  });
+  new Vue({
+    el: '#app3',
+    data() {
+      return { 
+       
+       
+        
+          image3:{
+            id:3,
+            src:"./media/pubg.png",
+            alt:"https://store.steampowered.com/widget/578080/" ,
+            src2:"./media/pubg.png"
+          },
+          
+          
+      }
+    },
+    
+    
+  });
+  new Vue({
+    el: '#app4',
+    data() {
+      return { 
+       
+       
+        
+          image4:{
+            id:4,
+            src:"./media/bg3.png",
+            alt:"https://store.steampowered.com/widget/1086940/" ,
+            src2:"./media/bg3.png"
+          },
+         
+          
+      }
+    },
+    
+    
+  });
+  new Vue({
+    el: '#app5',
+    data() {
+      return { 
+       
+       
+        
+          image5:{
+            id:5,
+            src:"./media/victoria3.jpg",
+            alt:"https://store.steampowered.com/widget/529340/754114/" ,
+            src2:"./media/victoria3.jpg"
+          },
+          
+      }
+    },
+    
+    
+  });
